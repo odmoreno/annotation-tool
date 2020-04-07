5617bb366f37089b237d844762cb7733685b11f2
Oscar <oscarpol95@gmail.com>
2020-04-07 03:08:07 -0500

    Add .env

--- src/annotations/components/FeatureList.js
+++ src/annotations/components/FeatureList.js
@@ -33,32 +33,32 @@ const FeatureList = props => {
     {
       'titulo': '2097',
       'name': '2097.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586221997/2097_vvhe4h.mp4'
+      'url': process.env.REACT_APP_URL_2097
     },
     {
       'titulo': '2085',
       'name': '2085.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586222045/2085_v9vi2q.mp4'
+      'url': process.env.REACT_APP_URL_2085
     },
     {
       'titulo': '2083',
       'name': '2083.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586245885/2083_qpidgz.mp4'
+      'url': process.env.REACT_APP_URL_2083
     },
     {
       'titulo': '2074',
       'name': '2074.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586245877/2074_guse0e.mp4'
+      'url': process.env.REACT_APP_URL_2074
     },
     {
       'titulo': '2096',
       'name': '2096.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586245870/2096_vfaqet.mp4'
+      'url': process.env.REACT_APP_URL_2096
     },
     {
       'titulo': '2077',
       'name': '2077.mp4',
-      'url': 'https://res.cloudinary.com/dl9owe03r/video/upload/v1586245840/2077_jgq7ns.mp4'
+      'url': process.env.REACT_APP_URL_2077
     }
   ];
 
